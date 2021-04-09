58c1ae1b350b52fb636d90d23070b79c2290050a
João Silva <joaoandre.m.silva@gmail.com>
2021-04-09 18:12:59 +0100

    Converted all line endings to Unix-style.

--- ContributedPlugins/ExtendPaste/ExtendPasteDialog.designer.cs
+++ ContributedPlugins/ExtendPaste/ExtendPasteDialog.designer.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright 2015 yoshikixxxx.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
--- Plugin/VersionInfo.cs
+++ Plugin/VersionInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2019 The Poderosa Project.
+// Copyright 2004-2019 The Poderosa Project.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
